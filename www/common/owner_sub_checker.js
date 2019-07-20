275ec4b773a673d6d5cb527fc85ef15a7f2170d2
Karl Vincent Lim <karllim96@gmail.com>
2019-07-21 01:26:31 +0800

    fixes

--- www/common/owner_sub_checker.js
+++ www/common/owner_sub_checker.js
@@ -14,4 +14,20 @@ if (CrapiApp.config.login_data.role == USER_ROLE.owner) {
 	.always(function(){
 		CrapiApp.removeCommonLoader();
 	});
+} else if (CrapiApp.config.login_data.role == USER_ROLE.employee) {
+	CrapiApp.getOwnerInfo({
+		id: CrapiApp.config.login_data.owner_id
+	})
+	.then(function(res){
+		console.log(res);
+
+		if (res.result === 'OK') {
+			if (res.data.is_expired) {
+				location.href = 'e-sub.html';
+			}
+		}
+	})
+	.always(function(){
+		CrapiApp.removeCommonLoader();
+	});
 }
