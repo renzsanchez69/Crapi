caea0e31450afcde2f60f9ca4a879d541b47e50d
Karl Lim <fdc.karllim96@gmail.com>
2019-07-23 16:13:21 +0800

    fix

--- platforms/browser/www/common/app.js
+++ platforms/browser/www/common/app.js
@@ -707,5 +707,12 @@ var CrapiApp = (function() {
 		}
 		return obj;
 	}
+	
+	obj.uuidv4 = function(obj){
+		return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+			var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+			return v.toString(16);
+		});
+	}
 	return obj;
 })();
